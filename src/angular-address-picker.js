37c45603d541fe5900ae945d881798c7367ceb4e
Ali Elderov <elderov@fundsaccess.com>
2016-12-18 19:59:31 +0100

    refactoring src/angular-address-picker.js

--- src/angular-address-picker.js
+++ src/angular-address-picker.js
@@ -1,43 +1,45 @@
-/*! angular-address-picker - v0.0.7
-* Copyright (c) G. Tomaselli <girotomaselli@gmail.com> 2015; Licensed  
-*/
-angular.module('ByGiro.addressPicker', [])
-.directive('bgapAddressPicker', ['$window','$parse','$compile', function ($window, $parse, $compile) {
-	
-	contrFunction = ['$scope', '$timeout', '$element', '$attrs', '$parse', function($scope, $timeout, $element, $attrs, $parse){
-			
-			$element.addressPickerByGiro($scope.options)
-			.on('selected.addressPickerByGiro', function(eve, data){
-				
-				if(!data.cleanData) return;
-				
-				if($attrs.ngModel){
-					$scope.ngModel = data.cleanData.formatted_address;
-				}
-				
-				if($scope.options.addressComponentsKey){
-					if($scope.$parent.model){
-						var otherKey = $scope.options.addressComponentsKey,
-						modelGetter = $parse(otherKey);
+(function() {
+    /*! ng-google-address-picker
+     * Copyright (c) G. Tomaselli <girotomaselli@gmail.com> 2015; Licensed MIT
+     */
+    angular.module('ByGiro.addressPicker', [])
+        .directive('bgapAddressPicker', ['$window', '$parse', '$compile', function($window, $parse, $compile) {
 
-						// This returns a function that lets us set the value of the ng-model binding expression:
-						var modelSetter = modelGetter.assign;
+            contrFunction = ['$scope', '$timeout', '$element', '$attrs', '$parse', function($scope, $timeout, $element, $attrs, $parse) {
 
-						// This is how you can use it to set the value on the given scope.
-						modelSetter($scope.$parent.model, data.cleanData);
-					}					
-				}
+                $element.addressPickerByGiro($scope.options)
+                    .on('selected.addressPickerByGiro', function(eve, data) {
 
-				$timeout();
-			});
-		}];
-	
-	return({
-		scope: {
-			options: "=bgapOptions",
-			ngModel: "=?"
-		},
-		restrict: "A",
-		controller: contrFunction
-	});
-}]);
+                        if (!data.cleanData) return;
+
+                        if ($attrs.ngModel) {
+                            $scope.ngModel = data.cleanData.formatted_address;
+                        }
+
+                        if ($scope.options.addressComponentsKey) {
+                            if ($scope.$parent.model) {
+                                var otherKey = $scope.options.addressComponentsKey,
+                                    modelGetter = $parse(otherKey);
+
+                                // This returns a function that lets us set the value of the ng-model binding expression:
+                                var modelSetter = modelGetter.assign;
+
+                                // This is how you can use it to set the value on the given scope.
+                                modelSetter($scope.$parent.model, data.cleanData);
+                            }
+                        }
+
+                        $timeout();
+                    });
+            }];
+
+            return ({
+                scope: {
+                    options: "=bgapOptions",
+                    ngModel: "=?"
+                },
+                restrict: "A",
+                controller: contrFunction
+            });
+        }]);
+})();
